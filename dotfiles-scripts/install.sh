02c30bc61d3e29b988232aa58aadcb454a47ed3c
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2021-12-23 15:49:34 +0100

    Update process

--- dotfiles-scripts/install.sh
+++ dotfiles-scripts/install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 cd $HOME
-echo ".cfg" >> .gitignore
-git clone https://github.com/lrivallain/dotfiles.git $HOME/.cfg
+
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
 shopt -s expand_aliases
-dotfiles config --local status.showUntrackedFiles no
-dotfiles checkout -f
+
+if [[ -d $HOME/.cfg/.git ]]
+then
+  echo "Already installed: lets update it"
+  dotfiles pull
+else
+  echo ".cfg" >> .gitignore
+  git clone https://github.com/lrivallain/dotfiles.git $HOME/.cfg
+  dotfiles config --local status.showUntrackedFiles no
+  dotfiles checkout -f
+fi
 
 bash $HOME/dotfiles-scripts/zsh-setup.sh
 bash $HOME/dotfiles-scripts/tools-setup-apt.sh
