dec1bd5e915fb0fe69a059ee8b78078d851b7430
Ludovic Rivallain <lrivallain@users.noreply.github.com>
2025-10-01 15:32:14 +0200

    Add OS detection for setup script execution

--- .dotfiles-scripts/install.sh
+++ .dotfiles-scripts/install.sh
@@ -17,4 +17,20 @@ else
 fi
 
 bash $HOME/.dotfiles-scripts/zsh-setup.sh
-bash $HOME/.dotfiles-scripts/tools-setup-apt.sh
+
+OS_TYPE="$(uname)"
+if [[ "$OS_TYPE" == "Linux" ]]; then
+    echo "Running on Linux"
+    if command -v apt >/dev/null 2>&1; then
+      bash $HOME/.dotfiles-scripts/tools-setup-apt.sh
+  else
+      echo "apt is NOT available: leaving"
+      exit 2
+  fi
+elif [[ "$OS_TYPE" == "Darwin" ]]; then
+    echo "Running on macOS"
+    bash $HOME/.dotfiles-scripts/tools-setup-mac.sh
+else
+    echo "Unknown OS: $OS_TYPE"
+    exit 1
+fi
